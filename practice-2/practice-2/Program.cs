c3d5c4ca6f1eebf3ca0c80597e299695636770d8
HwanHsuanChang <f74052293@mail.ncku.edu.tw>
2018-09-18 18:27:50 +0800

    Add files via upload

---
+++ practice-2/practice-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入矩陣一(輸入空行完成):");
+            int[,] x;
+            x = new int[10,10];
+            string a;
+            for(int i = 0;i < 10; ++i)
+            {
+                for (int j = 0; j < 10; ++j) {
+                    a = Console.ReadLine();
+                    if (a == null)
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("請輸入矩陣二(輸入空行完成):");
+
+            int[,] y;
+            y = new int[10,10];
+
+        }//main
+    }
+}
